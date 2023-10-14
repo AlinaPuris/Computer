f87a35590d81e166cdcf122fb5dc266304c9a1f9
Senior Java Develope <alina.puris@cnas.gov.md>
2023-10-15 00:00:14 +0300

    Initial commit

---
+++ src/edu/tekwill/academy/computer/Computer.java
@@ -0,0 +1,14 @@
+package edu.tekwill.academy.computer;
+
+import edu.tekwill.academy.computer.components.*;
+import edu.tekwill.academy.computer.peripherals.*;
+
+
+public class Computer {
+    Processor processor;
+    Motherboard motherboard;
+    HardDrive hardDrive;
+    Monitor monitor;
+    Mouse mouse;
+    Keyboard keyboard;
+}
